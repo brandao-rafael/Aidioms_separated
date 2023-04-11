b4524fde2623230a2a44999ea51909def0977bab
Rafael Brandão <rafaelbrandao1992@gmail.com>
2023-04-11 13:16:35 -0300

    Update env.js

--- frontend/public/env.js
+++ frontend/public/env.js
@@ -1,5 +1,5 @@
 window.env = {
-  "BASE_URL": "http://localhost:3006/",
+  "BASE_URL": "https://aidiomsv10-backend-production.up.railway.app",
   "X_API_KEY": "banana",
   "PS_BASE_URL": "http://localhost:5000/"
 };
