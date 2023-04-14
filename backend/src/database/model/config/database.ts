3d67b3efbd0dc1bf2c169acd5124d91b17e3b99a
Rafael Brandão <rafaelbrandao1992@gmail.com>
2023-04-14 16:18:24 -0300

    refactor: removing database hard to env

--- backend/src/database/model/config/database.ts
+++ backend/src/database/model/config/database.ts
@@ -5,7 +5,7 @@ import { Options } from 'sequelize';
 const config: Options = {
   username: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE || 'JetLabs',
+  database: process.env.MYSQLDATABASE,
   host: process.env.MYSQLHOST,
   port: Number(process.env.MYSQLPORT),
   dialect: 'mysql',
