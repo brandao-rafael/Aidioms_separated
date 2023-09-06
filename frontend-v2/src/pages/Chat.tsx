b99ba44a7ed079f4743d07a655ee2713d5017229
Rafael Brandão <rafaelbrandao1992@gmail.com>
2023-09-06 15:26:52 -0300

    feat: create initial components

--- frontend-v2/src/pages/Chat.tsx
+++ frontend-v2/src/pages/Chat.tsx
@@ -1,10 +1,8 @@
-import SideBar from "@/components/sideBar";
 import React from "react";
 
 const Chat: React.FC = () => {
   return (
     <div>
-      <SideBar />
       <h1>Chat</h1>
     </div>
   );
---
+++ frontend-v2/src/pages/imageGenerator.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ImageGenerator: React.FC = () => {
+  return (
+    <div>
+      <h1>Image</h1>
+    </div>
+  );
+};
+
+export default ImageGenerator;
